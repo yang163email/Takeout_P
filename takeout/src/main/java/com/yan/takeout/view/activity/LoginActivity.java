c532a37ce1ee08a205f3a1583aecd40a19bfe4b6
yang163email <13640662396@163.com>
2017-06-06 20:18:04 +0800

    倒计时的实现

--- takeout/src/main/java/com/yan/takeout/view/activity/LoginActivity.java
+++ takeout/src/main/java/com/yan/takeout/view/activity/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.yan.takeout.view.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -73,12 +76,46 @@ public class LoginActivity extends AppCompatActivity {
             case R.id.tv_user_code:
                 //发送获取验证码
                 SMSSDK.getVerificationCode("86", phone);
+                mTvUserCode.setEnabled(false);
+                new Thread(new CutDownTask()).start();
                 break;
             case R.id.login:
                 //登录验证
                 String code = mEtUserCode.getText().toString().trim();
                 SMSSDK.submitVerificationCode("86", phone, code);
                 break;
+        }
+    }
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case TIME_MAX:
+                    mTvUserCode.setText("剩余时间(" + time +")秒");
+                    break;
+                case TIME_IS_OUT:
+                    mTvUserCode.setText("重新获取");
+                    mTvUserCode.setEnabled(true);
+                    time = 60;
+                    break;
+            }
+        }
+    };
+
+    private int time = 60;
+    private static final int TIME_MAX = 0;
+    private static final int TIME_IS_OUT = 1;
+
+    private class CutDownTask implements Runnable {
+        @Override
+        public void run() {
+            for (; time > 0; time--) {
+                SystemClock.sleep(999);
+                mHandler.sendEmptyMessage(TIME_MAX);
+            }
+            mHandler.sendEmptyMessage(TIME_IS_OUT);
         }
     }
 }
