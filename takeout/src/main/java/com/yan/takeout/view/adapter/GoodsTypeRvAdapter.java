426409adddff7d2b122871e7c8bfb0b42682d9f5
yang163email <13640662396@163.com>
2017-06-08 21:26:59 +0800

    左侧切换选中条目

--- takeout/src/main/java/com/yan/takeout/view/adapter/GoodsTypeRvAdapter.java
+++ takeout/src/main/java/com/yan/takeout/view/adapter/GoodsTypeRvAdapter.java
@@ -1,6 +1,8 @@
 package com.yan.takeout.view.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -43,7 +45,7 @@ public class GoodsTypeRvAdapter extends RecyclerView.Adapter {
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         ViewHolder viewHolder = (ViewHolder) holder;
-        viewHolder.setData(mGoodsTypeInfoList.get(position));
+        viewHolder.setData(mGoodsTypeInfoList.get(position), position);
     }
 
     @Override
@@ -51,19 +53,42 @@ public class GoodsTypeRvAdapter extends RecyclerView.Adapter {
         return mGoodsTypeInfoList.size();
     }
 
-
+    /**当前选中的位置*/
+    private int mSelectedPosition = 0;
     class ViewHolder extends RecyclerView.ViewHolder{
+        private final View mView;
         @Bind(R.id.tvCount)
         TextView mTvCount;
         @Bind(R.id.type)
         TextView mType;
+        private int mPosition;
 
         ViewHolder(View view) {
             super(view);
+            mView = view;
             ButterKnife.bind(this, view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mSelectedPosition = mPosition;
+                    notifyDataSetChanged();
+                }
+            });
         }
 
-        public void setData(GoodsTypeInfo goodsTypeInfo) {
+        public void setData(GoodsTypeInfo goodsTypeInfo, int position) {
+            mPosition = position;
+
+            if(mSelectedPosition == mPosition) {
+                //选中效果
+                mView.setBackgroundColor(Color.WHITE);
+                mType.setTextColor(Color.BLACK);
+                mType.setTypeface(Typeface.DEFAULT_BOLD);
+            }else {
+                mView.setBackgroundColor(Color.parseColor("#b9dedcdc"));
+                mType.setTextColor(Color.GRAY);
+                mType.setTypeface(Typeface.DEFAULT);
+            }
             mType.setText(goodsTypeInfo.getName());
         }
     }
