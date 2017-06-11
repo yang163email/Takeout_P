788976ed5fc4a2c45fb8813a956530ad65b8c7eb
yang163email <13640662396@163.com>
2017-06-11 21:56:06 +0800

    地图的helloworld

---
+++ testmap/src/main/java/com/yan/testmap/MainActivity.java
@@ -0,0 +1,62 @@
+package com.yan.testmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+    private AMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.map_view);
+        mMapView.onCreate(savedInstanceState);
+
+        init();
+    }
+
+    private void init() {
+        if(mMap == null) {
+            mMap = mMapView.getMap();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+}
